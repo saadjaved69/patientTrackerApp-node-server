f5c282e1169c27a66807c034508cd1d820d8f99b
saad javed <saad.amjad2983035@gmail.com>
2019-12-09 03:59:53 +0500

    'saad'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cors = require('cors')
 
 app.use(cors());
 app.use(express.json());
-app.use('/user' , profileRoutes)
+app.use( profileRoutes)
 app.use(patientRoutes)
 // app.use(wishListRoutes)
 
